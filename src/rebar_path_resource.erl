1a47d06a19eb17277d37669c63c343a2101e2753
Hans Svensson <hanssv@gmail.com>
2024-09-20 11:53:30 +0200

    Change to MD5 checksum instead of last_modified
    Should be more stable when multiple devs work on the same repo...

--- src/rebar_path_resource.erl
+++ src/rebar_path_resource.erl
@@ -24,7 +24,8 @@ lock_(Dir, {path, Path, _}) ->
 lock_(_Dir, {path, Path}) ->
   {ok, Cwd} = file:get_cwd(),
   Source = filename:join([Cwd, Path]),
-  {path, Path, {mtime, to_iso8601(last_modified(Source))}}.
+  Checksum = dep_checksum(Source),
+  {path, Path, {md5, Checksum}}.
 
 download(TmpDir, {path, Path, _}, State) ->
   download(TmpDir, {path, Path}, State);
@@ -46,74 +47,70 @@ download_(Dir, {path, Path}, _State) ->
   ok = filelib:ensure_dir(Dir),
   {ok, Cwd} = file:get_cwd(),
   Source = filename:join([Cwd, Path]),
+
   Files = files(Source),
-  rebar_api:info("Download files: ~p" , [Files]),
+
   lists:foreach(fun(File) -> copy(File, Source, Dir) end, Files),
-  rebar_log:log(debug, "copied source from=~p, to=~p ~n", [Path, Dir]),
-  LastModified = last_modified(Source, Files),
-  {ok, A} = file:read_file_info(Dir),
-  file:write_file_info(Path, A#file_info{mtime = LastModified, atime = LastModified}).
+  rebar_api:debug("copied source from=~p, to=~p ~n", [Path, Dir]),
+  ok.
 
 make_vsn(_Dir) ->
-  {error, "Replacing version of type path not supported."}.
+  {error, "Replacing version of type `path` not supported."}.
 
 %% Let's only target rebar3 version > 3.7.0 when the resource API was defined.
 needs_update(AppInfo, _CustomState) ->
   needs_update_(rebar_app_info:dir(AppInfo), rebar_app_info:source(AppInfo)).
 
-needs_update_(Dir, {path, Path, _}) ->
+needs_update_(_Dir, {path, Path, {md5, OldChecksum}}) ->
   {ok, Cwd} = file:get_cwd(),
   Source = filename:join([Cwd, Path]),
-  LastModified = last_modified(Source),
-  Old = filelib:last_modified(Dir),
-  rebar_log:log(debug, "compare dir=~p, path=~p last modified=~p, old=~p~n", [Dir, Path, LastModified, Old]),
-  (Old < LastModified).
-
-last_modified(Source) ->
-  Files = files(Source),
-  last_modified(Source, Files).
-
-last_modified(_, []) -> calendar:local_time();
-last_modified(Root, Files) ->
-  lists:max([ filelib:last_modified(filename:join(Root, File)) || File <- Files ]).
-
-to_iso8601({{Y,Mo,D}, {H,Mn,S}}) ->
-    FmtStr = "~4.10.0B-~2.10.0B-~2.10.0BT~2.10.0B:~2.10.0B:~2.10.0BZ",
-    IsoStr = io_lib:format(FmtStr, [Y, Mo, D, H, Mn, S]),
-    list_to_binary(IsoStr).
+  Checksum = dep_checksum(Source),
+  %% rebar_api:info("Checksum ~p =?= ~200p", [Checksum, OldChecksum]),
+  OldChecksum /= Checksum;
+needs_update_(_Dir, Path) ->
+  rebar_api:debug("Unexpected path format: ~p - expected {path, <Path>, {md5, <Checksum>}}", Path),
+  true.
 
 copy(File, Source, Target) ->
-    SourceFile = filename:join(Source, File),
-    TargetFile = filename:join(Target, File),
-    ok = filelib:ensure_dir(TargetFile),
-    {ok, _} = file:copy(SourceFile, TargetFile).
+  SourceFile = filename:join(Source, File),
+  TargetFile = filename:join(Target, File),
+  ok = filelib:ensure_dir(TargetFile),
+  {ok, _} = file:copy(SourceFile, TargetFile).
+
+dep_checksum(Source) ->
+  MD5s = [ md5_file(filename:join(Source, File)) || File <- files(Source) ],
+  Checksum = erlang:md5(<< MD5 || <<MD5/binary>> <- MD5s>>),
+  binary:encode_hex(Checksum, lowercase).
+
+md5_file(File) ->
+  case file:read_file(File) of
+    {ok, Bin} -> erlang:md5(Bin);
+    _         -> <<>>
+  end.
 
 -define(EXCLUDE_PATHS, [".git", "_build", "examples", ".eqc_info"]).
 -define(EXCLUDE_FILES, ["^\\.", "~$", "current_counterexample.eqc"]).
 
 %% Return the list of files - either for copy, or update check/checksum
 files(RootDir) ->
-    files(RootDir, ?EXCLUDE_PATHS, ?EXCLUDE_FILES, ".").
+  files(RootDir, ?EXCLUDE_PATHS, ?EXCLUDE_FILES, ".").
 
 files(Root, IgnorePaths, IgnoreFiles, Path) ->
-    File = filename:join(Root, Path),
-    case filelib:is_dir(File) of
-        true  ->
-            case file:list_dir(File) of
-                {ok, List} ->
-                    Dirs = [filename:join(Path, X) || X <- List, not lists:member(X, IgnorePaths)],
-                    lists:flatmap(fun(X) -> files(Root, IgnorePaths, IgnoreFiles, X) end, Dirs);
-                {error, _Reason} ->
-                    []
-            end;
-        false ->
-            case is_excluded(filename:basename(File), IgnoreFiles) of
-                true -> [];
-                false -> [Path]
-            end
-    end.
+  File = filename:join(Root, Path),
+  case filelib:is_dir(File) of
+    true  ->
+      case file:list_dir(File) of
+        {ok, List} ->
+          Dirs = [filename:join(Path, X) || X <- List, not lists:member(X, IgnorePaths)],
+          lists:flatmap(fun(X) -> files(Root, IgnorePaths, IgnoreFiles, X) end, Dirs);
+        {error, _Reason} ->
+          []
+      end;
+    false ->
+      [ Path || not is_excluded(filename:basename(File), IgnoreFiles) ]
+  end.
 
 is_excluded(Path, Excludes) ->
-    lists:foldl(fun(_, true) -> true;
-                   (RE, false) -> re:run(Path, RE, [unicode]) =/= nomatch
-                end, false, Excludes).
+  lists:foldl(fun(_, true)   -> true;
+                 (RE, false) -> re:run(Path, RE, [unicode]) =/= nomatch
+              end, false, Excludes).
